8ea8a49fae26109f98a4748b500bb58a5ca41791
SophieLiu071117 <123265813+SophieLiu071117@users.noreply.github.com>
2025-10-04 20:04:55 +0000

    "Made the map with a pin"

--- my-react-app/src/Map.jsx
+++ my-react-app/src/Map.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import './Map.css';
+import React from "react";
+import "./Map.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+// 1. Import L from Leaflet
+import L from "leaflet";
+
+// 2. Define the custom icon
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 function Map() {
   return (
     <div className="map-container">
       <h1>Map Page</h1>
-      {/* Map content will go here */}
+
+      <MapContainer
+        center={[43.788991157897776, -79.19059949394783]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[43.78754535704544, -79.19009374471229]}
+          // 3. Pass the custom icon to the Marker
+          icon={redIcon}
+        >
+          <Popup>You collected a trash item here!</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
